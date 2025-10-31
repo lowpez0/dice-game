6d29033377119499db2871b1052dc8c7cc3b2d31
lowpez <sadscientist101@gmail.com>
2025-10-31 18:08:47 +0800

    slight changes

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Happy developing ✨')
+console.log('sadsdasdqq')
+const btnRollDice = document.querySelector('#btn-rd');
+const btnHoldDice = document.querySelector('#btn-ho');
+const dice = document.querySelector('.dice');
+let holdScore = 0;
+
+
+const p1 = {
+    score: 0,
+    turn: true
+}
+
+const p2 = {
+    score: 0,
+    turn: false
+}
+
+const rollDice = () => {
+    const randomDice = Math.floor(Math.random() * 6) + 1;
+    dice.src = `dice-${randomDice}.png`;
+    dice.classList.remove('hidden');
+        
+    
+    
+}
+
+const holdDice = () => {
+   
+}
+
+
